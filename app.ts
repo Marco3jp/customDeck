236e9fc8006bbf72f7111c7f08fba4d84b8f2050
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-25 19:43:15 +0900

    TweetDeckの新しいNewTweet部分にスタイルシートをインジェクションする実装をした

---
+++ app.ts
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         forMeDeck
+// @namespace    https://marco.plus
+// @version      0.1
+// @description  This is TweetDeck custom script for me.
+// @author       Marco
+// @match        https://tweetdeck.twitter.com*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    let newTweetIframeStyleSheet = ":root{--original-white:rgba(250,250,250,1);--original-gray:rgba(158,158,158,1);--original-black:rgba(33,33,33,1);--Dark:rgba(27,40,54,1);--deep-dark:rgba(61,84,102,1);--light-dark:rgba(96,125,139,1)}body{overflow-y:hidden}.css-1dbjc4n.r-1awozwy.r-1iusvr4.r-16y2uox.r-4amgru.r-8kwpq0.r-bcqeeo{display:none}.notranslate.public-DraftEditor-content{font-size:14px;font-family:\"Roboto\",\"Yu Gothic Medium\",YuGothic,'ヒラギノ角ゴ Pro W3','Hiragino Kaku Gothic Pro','メイリオ',Meiryo,'ＭＳ Ｐゴシック','MS PGothic',sans-serif!important;padding:10px!important;background-color:var(--light-dark)!important;border-radius:5px}[style=\"color: rgb(27, 149, 224);\"]{color:rgba(128,216,255,1)!important}";
+
+    function init() {
+        const startTime = Date.now();
+        let config = {childList: true, subtree: true};
+        let getNewTweetColumn = function (mutationsList, columnObserver) {
+            let drawer = document.querySelector(".js-drawer");
+            if (drawer !== null) {
+                columnObserver.disconnect();
+                let getIframeCallback = function (mutationsList, iframeObserver) {
+                    if (document.querySelector(".r-1yadl64.r-16y2uox") !== null) {
+                        iframeObserver.disconnect();
+                        injectStyleSheetInNewTweetIframe();
+                    } else if (startTime + 1000 * 30 < Date.now()) { // timeout 30sec
+                        iframeObserver.disconnect();
+                        tweetDeckAlert("Timeout: Cannot find NewTweetElement.");
+                    }
+                };
+                let iframeObserver = new MutationObserver(getIframeCallback);
+                iframeObserver.observe(drawer, config);
+            } else if (startTime + 1000 * 30 < Date.now()) { // timeout 30sec
+                columnObserver.disconnect();
+                tweetDeckAlert("Timeout: Cannot find NewTweetColumn.");
+            }
+        };
+
+        let columnObserver = new MutationObserver(getNewTweetColumn);
+        columnObserver.observe(document.body, config);
+    }
+
+    function injectStyleSheetInNewTweetIframe() {
+        const newTweetIframe: HTMLIFrameElement = document.querySelector(".r-1yadl64.r-16y2uox");
+        if (newTweetIframe === null) {
+            tweetDeckAlert("Not available: This forMeDeck version is incompatible latest TweetDeck version.");
+        } else {
+            const styleElm = document.createElement("style");
+            styleElm.textContent = newTweetIframeStyleSheet;
+            newTweetIframe.contentDocument.querySelector("head").appendChild(styleElm);
+        }
+    }
+
+    function tweetDeckAlert(message) {
+        const divElm = document.createElement("div");
+        const spanElm = document.createElement("span");
+        spanElm.textContent = message;
+        divElm.classList.add("deckAlertWrapper");
+        spanElm.classList.add("deckAlert");
+        divElm.appendChild(spanElm);
+        document.body.appendChild(divElm);
+        setTimeout(() => {
+            document.body.removeChild(divElm);
+        }, 3000)
+    }
+
+    init();
+})();
